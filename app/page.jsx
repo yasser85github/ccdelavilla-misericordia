6dd97c510dceca8c44ed04486b3d09fa05620c21
Yasser Vazquez <yasser.vazquez85@gmail.com>
2023-10-21 22:56:33 +0100

    more things changed

--- app/page.jsx
+++ app/page.jsx
@@ -2,22 +2,22 @@ import RemoveBtn from "@/components/RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-import Person from "@/components/Person";
+// import Person from "@/components/Person";
 
-const getPeople = async () => {
-  const apiUrl = process.env.API_URL;
-  try {
-    const res = await fetch(`${apiUrl}/api/people`, {
-      cache: "no-store",
-    });
-    if (!res.ok) {
-      throw new Error("Failed to fetch people");
-    }
-    return res.json();
-  } catch (error) {
-    console.log("Error loading people:", error);
-  }
-};
+// const getPeople = async () => {
+//   const apiUrl = process.env.API_URL;
+//   try {
+//     const res = await fetch(`${apiUrl}/api/people`, {
+//       cache: "no-store",
+//     });
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch people");
+//     }
+//     return res.json();
+//   } catch (error) {
+//     console.log("Error loading people:", error);
+//   }
+// };
 
 export default async function PeopleList() {
   const { people } = await getPeople();
